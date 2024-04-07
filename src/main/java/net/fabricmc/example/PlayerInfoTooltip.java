a4ed98cbbc9f19fdf6c4cda7a3784a1d63691b94
Jakob <minenash@protonmail.com>
2024-04-07 17:21:28 -0400

    Add air, xp, vehicle info (name, health, armor)

--- src/main/java/net/fabricmc/example/PlayerInfoTooltip.java
+++ src/main/java/net/fabricmc/example/PlayerInfoTooltip.java
@@ -1,12 +1,16 @@
 package net.fabricmc.example;
 
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.MathHelper;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static net.minecraft.util.math.MathHelper.ceil;
+import static net.minecraft.util.math.MathHelper.floor;
 
 public class PlayerInfoTooltip {
 
@@ -14,21 +18,45 @@ public class PlayerInfoTooltip {
     public static final Style DEFAULT = Style.EMPTY.withFont(Style.DEFAULT_FONT_ID);
 
     public static List<Text> tooltip(ClientPlayerEntity p) {
-        int health = MathHelper.ceil(p.getHealth() + p.getAbsorptionAmount());
+        int health = ceil(p.getHealth() + p.getAbsorptionAmount());
         int armor = p.getArmor();
         int hunger = p.getHungerManager().getFoodLevel();
-        int sat = MathHelper.ceil(p.getHungerManager().getSaturationLevel());
-        boolean shift = health > 9 || armor > 9 || hunger > 9 || sat > 9;
+        int sat = ceil(p.getHungerManager().getSaturationLevel());
+        boolean pShift = health > 9 || armor > 9 || hunger > 9 || sat > 9;
 
-        return List.of( Text.literal("ⓘ ").append(p.getDisplayName()),
-                line(shift, "\uEFF1", health, MathHelper.ceil(p.getMaxHealth()) ),
-                line(shift, "\uEFF2", armor, 20 ),
-                line(shift, "\uEFF3", hunger, 20 ),
-                line(shift, "\uEFF4", sat, 20) );
+        String air = Math.max(0, ceil(100D * p.getAir() / p.getMaxAir())) + "%";
+        String xp = p.experienceLevel + "L " + floor(p.experienceProgress * p.getNextLevelExperience()) + " / " + p.getNextLevelExperience();
+
+        LivingEntity v = p.getVehicle() instanceof LivingEntity le ? le : null;
+
+        ArrayList<Text> lines = new ArrayList<>(9);
+        lines.add( Text.literal("ⓘ ").append(p.getDisplayName()) );
+        lines.add( line(pShift, "\uEFF1", health, ceil(p.getMaxHealth())) );
+        lines.add( line(pShift, "\uEFF2", armor, 20) );
+        lines.add( line(pShift, "\uEFF3", hunger, 20) );
+        lines.add( line(pShift, "\uEFF4", sat, 20) );
+        lines.add( Text.literal("") );
+        lines.add( line("\uEFF5 ", air) );
+        lines.add( line( "\uEFF6 ", xp) );
+
+        if (v != null) {
+            int vHealth = ceil(v.getHealth() + v.getAbsorptionAmount());
+            boolean vShift = vHealth > 9 || v.getArmor() > 9;
+            lines.add( Text.literal("") );
+            lines.add( Text.literal("ⓘ ").append(v.getDisplayName()) );
+            lines.add( line(vShift, "\uEFF7", vHealth, ceil( v.getMaxHealth() )) );
+            lines.add( line(vShift, "\uEFF2", v.getArmor(), 11) );
+        }
+
+        return lines;
     }
 
     public static Text line(boolean shift, String icon, int value, int max) {
-        return Text.literal(icon + (shift && value < 10 ? " \uEFF5" : " ")).setStyle(ICONS).append(Text.literal(value + " / " + max).setStyle(DEFAULT));
+        return line(icon + (shift && value < 10 ? " \uEFFA" : " "), value + " / " + max);
+    }
+
+    public static Text line(String icon, String msg) {
+        return Text.literal(icon).setStyle(ICONS).append(Text.literal(msg).setStyle(DEFAULT));
     }
 
 }
---
+++ src/main/java/net/fabricmc/example/mixin/HorseScreenMixin.java
@@ -0,0 +1,29 @@
+package net.fabricmc.example.mixin;
+
+import net.fabricmc.example.PlayerInfoTooltip;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.screen.ingame.HorseScreen;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.screen.HorseScreenHandler;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(HorseScreen.class)
+public abstract class HorseScreenMixin extends HandledScreen<HorseScreenHandler> {
+
+
+    public HorseScreenMixin(HorseScreenHandler screenHandler, PlayerInventory playerInventory, Text text) {
+        super(screenHandler, playerInventory, text);
+    }
+
+    @Inject(method = "render", at = @At("TAIL"))
+    private void addPlayerTooltip(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        if (mouseX >= x+26 && mouseX <= x+26+52 && mouseY >= y+18 && mouseY <= y+18+52)
+            renderTooltip(matrices, PlayerInfoTooltip.tooltip(client.player), mouseX, mouseY);
+    }
+
+}
